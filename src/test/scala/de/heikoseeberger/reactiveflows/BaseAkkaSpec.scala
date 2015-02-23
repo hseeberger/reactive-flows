fe524e8c3731dca84f848b756d3156a9b992a729
Heiko Seeberger <mail@heikoseeberger.de>
2015-02-23 22:20:35 +0100

    Skeleton HTTP service

---
+++ src/test/scala/de/heikoseeberger/reactiveflows/BaseAkkaSpec.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2015 Heiko Seeberger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.heikoseeberger.reactiveflows
+
+import akka.actor.ActorSystem
+import org.scalatest.{ BeforeAndAfterAll, Matchers, WordSpec }
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+abstract class BaseAkkaSpec extends WordSpec with Matchers with BeforeAndAfterAll {
+
+  implicit protected val system = ActorSystem("reactive-flows-system")
+
+  override protected def afterAll() = {
+    Await.ready(system.terminate(), Duration.Inf)
+    super.afterAll()
+  }
+}
