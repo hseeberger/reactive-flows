aa71a118359b5810ada4a879a6bceb4124901838
Heiko Seeberger <mail@heikoseeberger.de>
2015-02-23 20:12:05 +0100

    Set up project

--- build.sbt
+++ build.sbt
@@ -1,12 +1,28 @@
 lazy val reactiveFlows = project
   .in(file("."))
+  .configs(MultiJvm)
   .enablePlugins(AutomateHeaderPlugin, GitVersioning)
 
 name := "reactive-flows"
 
 libraryDependencies ++= List(
-  Library.scalaCheck % "test",
-  Library.scalaTest  % "test"
+  Library.akkaActor,
+  Library.akkaCluster,
+  Library.akkaContrib,
+  Library.akkaDataReplication,
+  Library.akkaHttp,
+  Library.akkaLog4j,
+  Library.akkaHttpSprayJson,
+  Library.akkaSse,
+  Library.log4jCore,
+  Library.akkaHttpTestkit      % "test",
+  Library.akkaMultiNodeTestkit % "test",
+  Library.akkaTestkit          % "test",
+  Library.scalaTest            % "test"
 )
 
 initialCommands := """|import de.heikoseeberger.reactiveflows._""".stripMargin
+
+addCommandAlias("rf1", "reStart -Dakka.remote.netty.tcp.port=2551 -Dreactive-flows.http-service.port=9001")
+addCommandAlias("rf2", "run     -Dakka.remote.netty.tcp.port=2552 -Dreactive-flows.http-service.port=9002")
+addCommandAlias("rf3", "run     -Dakka.remote.netty.tcp.port=2553 -Dreactive-flows.http-service.port=9003")
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,18 +1,28 @@
 import sbt._
 
 object Version {
-  val akka       = "2.3.9"
-  val log4j      = "2.2"
-  val scala      = "2.11.6"
-  val scalaCheck = "1.12.1"
-  val scalaTest  = "2.2.4"
+  val akka                 = "2.3.11"
+  val akkaDataReplication  = "0.11"
+  val akkaHttp             = "1.0-RC3"
+  val akkaLog4j            = "0.2.0"
+  val akkaSse              = "0.13.0"
+  val log4j                = "2.3"
+  val scala                = "2.11.6"
+  val scalaTest            = "2.2.5"
 }
 
 object Library {
-  val akkaActor   = "com.typesafe.akka"        %% "akka-actor"   % Version.akka
-  val akkaTestkit = "com.typesafe.akka"        %% "akka-testkit" % Version.akka
-  val log4jApi    = "org.apache.logging.log4j" %  "log4j-api"    % Version.log4j
-  val log4jCore   = "org.apache.logging.log4j" %  "log4j-core"   % Version.log4j
-  val scalaCheck  = "org.scalacheck"           %% "scalacheck"   % Version.scalaCheck
-  val scalaTest   = "org.scalatest"            %% "scalatest"    % Version.scalaTest
+  val akkaActor            = "com.typesafe.akka"        %% "akka-actor"                     % Version.akka
+  val akkaCluster          = "com.typesafe.akka"        %% "akka-cluster"                   % Version.akka
+  val akkaContrib          = "com.typesafe.akka"        %% "akka-contrib"                   % Version.akka
+  val akkaDataReplication  = "com.github.patriknw"      %% "akka-data-replication"          % Version.akkaDataReplication
+  val akkaHttp             = "com.typesafe.akka"        %% "akka-http-experimental"         % Version.akkaHttp
+  val akkaHttpSprayJson = "com.typesafe.akka"        %% "akka-http-spray-json-experimental" % Version.akkaHttp
+  val akkaHttpTestkit      = "com.typesafe.akka"        %% "akka-http-testkit-experimental" % Version.akkaHttp
+  val akkaLog4j            = "de.heikoseeberger"        %% "akka-log4j"                     % Version.akkaLog4j
+  val akkaMultiNodeTestkit = "com.typesafe.akka"        %% "akka-multi-node-testkit"        % Version.akka
+  val akkaSse              = "de.heikoseeberger"        %% "akka-sse"                       % Version.akkaSse
+  val akkaTestkit          = "com.typesafe.akka"        %% "akka-testkit"                   % Version.akka
+  val log4jCore            = "org.apache.logging.log4j" %  "log4j-core"                     % Version.log4j
+  val scalaTest            = "org.scalatest"            %% "scalatest"                      % Version.scalaTest
 }
