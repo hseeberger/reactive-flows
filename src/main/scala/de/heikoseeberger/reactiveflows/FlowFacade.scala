45a3f91716e8f2fa938dd192386c7afc9f27506d
Heiko Seeberger <mail@heikoseeberger.de>
2015-07-27 19:18:07 +0200

    Akka actors - communication

--- src/main/scala/de/heikoseeberger/reactiveflows/FlowFacade.scala
+++ src/main/scala/de/heikoseeberger/reactiveflows/FlowFacade.scala
@@ -17,16 +17,52 @@
 package de.heikoseeberger.reactiveflows
 
 import akka.actor.{ Actor, Props }
+import java.net.URLEncoder
 
 object FlowFacade {
+
+  case object GetFlows
+  case class FlowDescriptor(name: String, label: String)
+
+  case class AddFlow(label: String)
+  case class FlowAdded(flowDescriptor: FlowDescriptor)
+  case class FlowExists(label: String)
+
+  case class RemoveFlow(name: String)
+  case class FlowRemoved(name: String)
+  case class FlowUnknown(name: String)
 
   // $COVERAGE-OFF$
   final val Name = "flow-facade"
   // $COVERAGE-ON$
 
   def props: Props = Props(new FlowFacade)
+
+  private def labelToName(label: String) = URLEncoder.encode(label.toLowerCase, "UTF-8")
 }
 
 class FlowFacade extends Actor {
-  override def receive = Actor.emptyBehavior
+  import FlowFacade._
+
+  private var flowsByName = Map.empty[String, FlowDescriptor]
+
+  override def receive = {
+    case GetFlows                                                    => sender() ! flowsByName.valuesIterator.to[Set]
+    case AddFlow(label) if !flowsByName.contains(labelToName(label)) => addFlow(label)
+    case AddFlow(label)                                              => sender() ! FlowExists(label)
+    case RemoveFlow(name) if flowsByName.contains(name)              => removeFlow(name)
+    case RemoveFlow(name)                                            => sender() ! FlowUnknown(name)
+  }
+
+  private def addFlow(label: String) = {
+    val name = labelToName(label)
+    val flowDescriptor = FlowDescriptor(name, label)
+    flowsByName += name -> flowDescriptor
+    sender() ! FlowAdded(flowDescriptor)
+  }
+
+  private def removeFlow(name: String) = {
+    flowsByName -= name
+    sender() ! FlowRemoved(name)
+  }
 }
