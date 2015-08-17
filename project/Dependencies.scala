03f0eb55c8c9ca3a8f67b68d056bdad1814671ad
Heiko Seeberger <mail@heikoseeberger.de>
2015-08-17 22:20:45 +0200

    Akka HTTP - complete service

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,18 +1,23 @@
 import sbt._
 
 object Version {
-  final val Akka       = "2.4.2"
-  final val AkkaLog4j  = "1.1.2"
-  final val Log4j      = "2.5"
-  final val Scala      = "2.11.7"
-  final val ScalaTest  = "2.2.6"
+  final val Akka         = "2.4.2"
+  final val AkkaHttpJson = "1.5.2"
+  final val AkkaLog4j    = "1.1.2"
+  final val Circe        = "0.3.0"
+  final val Log4j        = "2.5"
+  final val Scala        = "2.11.7"
+  final val ScalaTest    = "2.2.6"
 }
 
 object Library {
   val akkaHttp        = "com.typesafe.akka"        %% "akka-http-experimental" % Version.Akka
+  val akkaHttpCirce   = "de.heikoseeberger"        %% "akka-http-circe"        % Version.AkkaHttpJson
   val akkaHttpTestkit = "com.typesafe.akka"        %% "akka-http-testkit"      % Version.Akka
   val akkaLog4j       = "de.heikoseeberger"        %% "akka-log4j"             % Version.AkkaLog4j
   val akkaTestkit     = "com.typesafe.akka"        %% "akka-testkit"           % Version.Akka
+  val circeGeneric    = "io.circe"                 %% "circe-generic"          % Version.Circe
+  val circeJava8      = "io.circe"                 %% "circe-java8"            % Version.Circe
   val log4jCore       = "org.apache.logging.log4j" %  "log4j-core"             % Version.Log4j
   val scalaTest       = "org.scalatest"            %% "scalatest"              % Version.ScalaTest
 }
