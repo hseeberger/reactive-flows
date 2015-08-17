31bb449e3b4d257aacf6b7446321887bb0dcb0b8
Heiko Seeberger <mail@heikoseeberger.de>
2015-08-17 10:27:56 +0200

    Akka actors - death watch

--- src/main/scala/de/heikoseeberger/reactiveflows/package.scala
+++ src/main/scala/de/heikoseeberger/reactiveflows/package.scala
@@ -16,10 +16,14 @@
 
 package de.heikoseeberger
 
+import scala.reflect.{ ClassTag, classTag }
+
 package object reactiveflows {
 
   type Traversable[+A] = scala.collection.immutable.Traversable[A]
   type Iterable[+A] = scala.collection.immutable.Iterable[A]
   type Seq[+A] = scala.collection.immutable.Seq[A]
   type IndexedSeq[+A] = scala.collection.immutable.IndexedSeq[A]
+
+  def className[A: ClassTag]: String = classTag[A].runtimeClass.getName
 }
