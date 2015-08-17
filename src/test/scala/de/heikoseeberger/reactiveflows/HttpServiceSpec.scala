7d5e13e360d0804733a4dedce46240797d7d71b0
Heiko Seeberger <mail@heikoseeberger.de>
2015-08-17 22:20:45 +0200

    Akka HTTP - complete service

--- src/test/scala/de/heikoseeberger/reactiveflows/HttpServiceSpec.scala
+++ src/test/scala/de/heikoseeberger/reactiveflows/HttpServiceSpec.scala
@@ -16,46 +16,222 @@
 
 package de.heikoseeberger.reactiveflows
 
+import akka.actor.ActorRef
 import akka.http.scaladsl.client.RequestBuilding
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.RouteTest
 import akka.http.scaladsl.testkit.TestFrameworkInterface.Scalatest
-import akka.testkit.{ EventFilter, TestProbe }
+import akka.testkit.{ EventFilter, TestActor, TestProbe }
+import de.heikoseeberger.akkahttpcirce.CirceSupport
+import java.time.LocalDateTime
 import org.scalatest.{ Matchers, WordSpec }
-import scala.concurrent.duration.DurationInt
 
 class HttpServiceSpec extends WordSpec with Matchers with RouteTest with Scalatest with RequestBuilding {
+  import CirceCodec._
+  import CirceSupport._
   import HttpService._
+  import io.circe.generic.auto._
+
+  val settings = Settings(system)
+  import settings.httpService._
 
   "The HttpService route" should {
     "send itself a Stop upon a 'DELETE /' and respond with OK" in {
       val httpService = TestProbe()
+      val flowFacade = TestProbe()
 
       val request = Delete()
-      request ~> route(httpService.ref) ~> check {
+      request ~> route(httpService.ref, flowFacade.ref, flowFacadeTimeout) ~> check {
         response.status shouldBe StatusCodes.OK
       }
 
       httpService.expectMsg(Stop)
     }
 
-    "respond with OK and index.html upon a 'GET /'" in {
+    "respond with OK upon a 'GET /'" in {
       val httpService = TestProbe()
+      val flowFacade = TestProbe()
 
       val request = Get()
-      request ~> route(httpService.ref) ~> check {
+      request ~> route(httpService.ref, flowFacade.ref, flowFacadeTimeout) ~> check {
         response.status shouldBe StatusCodes.OK
-        responseAs[String].trim shouldBe "test"
       }
     }
 
-    "respond with OK and index.html upon a 'GET /index.html'" in {
+    "respond with OK upon a 'GET /index.html'" in {
       val httpService = TestProbe()
+      val flowFacade = TestProbe()
 
       val request = Get("/index.html")
-      request ~> route(httpService.ref) ~> check {
+      request ~> route(httpService.ref, flowFacade.ref, flowFacadeTimeout) ~> check {
         response.status shouldBe StatusCodes.OK
-        responseAs[String].trim shouldBe "test"
+      }
+    }
+
+    "ask the FlowFacade GetFlows and respond with OK with the proper payload upon a 'GET /flows'" in {
+      val httpService = TestProbe()
+      val flowFacade = TestProbe()
+      val akkaFlow = FlowFacade.FlowDescriptor("akka", "Akka")
+      val angularJsFlow = FlowFacade.FlowDescriptor("angularjs", "AngularJS")
+      val flows = Vector(akkaFlow, angularJsFlow)
+      flowFacade.setAutoPilot(new TestActor.AutoPilot {
+        def run(sender: ActorRef, msg: Any) = msg match {
+          case FlowFacade.GetFlows =>
+            sender ! flows
+            TestActor.NoAutoPilot
+        }
+      })
+
+      val request = Get("/flows")
+      request ~> route(httpService.ref, flowFacade.ref, flowFacadeTimeout) ~> check {
+        response.status shouldBe StatusCodes.OK
+        responseAs[Seq[FlowFacade.FlowDescriptor]] shouldBe flows
+      }
+    }
+
+    "ask the FlowFacade AddFlow and respond with Created with the proper payload upon a 'POST /flows' for an unknown Flow" in {
+      val httpService = TestProbe()
+      val flowFacade = TestProbe()
+      val flowAdded = FlowFacade.FlowAdded(FlowFacade.FlowDescriptor("akka", "Akka"))
+      flowFacade.setAutoPilot(new TestActor.AutoPilot {
+        def run(sender: ActorRef, msg: Any) = msg match {
+          case FlowFacade.AddFlow("Akka") =>
+            sender ! flowAdded
+            TestActor.NoAutoPilot
+        }
+      })
+
+      val request = Post("/flows", AddFlowRequest("Akka"))
+      request ~> route(httpService.ref, flowFacade.ref, flowFacadeTimeout) ~> check {
+        response.status shouldBe StatusCodes.Created
+        responseAs[FlowFacade.FlowAdded] shouldBe flowAdded
+      }
+    }
+
+    "ask the FlowFacade AddFlow and respond with Conflict with the proper payload upon a 'POST /flows' for an existing Flow" in {
+      val httpService = TestProbe()
+      val flowFacade = TestProbe()
+      val flowExists = FlowFacade.FlowExists("Akka")
+      flowFacade.setAutoPilot(new TestActor.AutoPilot {
+        def run(sender: ActorRef, msg: Any) = msg match {
+          case FlowFacade.AddFlow("Akka") =>
+            sender ! flowExists
+            TestActor.NoAutoPilot
+        }
+      })
+
+      val request = Post("/flows", AddFlowRequest("Akka"))
+      request ~> route(httpService.ref, flowFacade.ref, flowFacadeTimeout) ~> check {
+        response.status shouldBe StatusCodes.Conflict
+        responseAs[FlowFacade.FlowExists] shouldBe flowExists
+      }
+    }
+
+    "ask the FlowFacade RemoveFlow and respond with NoContent with the proper payload upon a 'DELETE /flows/akka' for an existing Flow" in {
+      val httpService = TestProbe()
+      val flowFacade = TestProbe()
+      flowFacade.setAutoPilot(new TestActor.AutoPilot {
+        def run(sender: ActorRef, msg: Any) = msg match {
+          case FlowFacade.RemoveFlow("akka") =>
+            sender ! FlowFacade.FlowRemoved("akka")
+            TestActor.NoAutoPilot
+        }
+      })
+
+      val request = Delete("/flows/akka")
+      request ~> route(httpService.ref, flowFacade.ref, flowFacadeTimeout) ~> check {
+        response.status shouldBe StatusCodes.NoContent
+      }
+    }
+
+    "ask the FlowFacade RemoveFlow and respond with NotFound with the proper payload upon a 'DELETE /flows/unknown' for an unknown Flow" in {
+      val httpService = TestProbe()
+      val flowFacade = TestProbe()
+      flowFacade.setAutoPilot(new TestActor.AutoPilot {
+        def run(sender: ActorRef, msg: Any) = msg match {
+          case FlowFacade.RemoveFlow("unknown") =>
+            sender ! FlowFacade.FlowUnknown("unknown")
+            TestActor.NoAutoPilot
+        }
+      })
+
+      val request = Delete("/flows/unknown")
+      request ~> route(httpService.ref, flowFacade.ref, flowFacadeTimeout) ~> check {
+        response.status shouldBe StatusCodes.NotFound
+      }
+    }
+
+    "ask the FlowFacade GetMessages and respond with OK with the proper payload upon a 'GET /flows/akka/messages' for an existing Flow" in {
+      val httpService = TestProbe()
+      val flowFacade = TestProbe()
+      val messages = Vector(Flow.Message("Akka rules!", now()))
+      flowFacade.setAutoPilot(new TestActor.AutoPilot {
+        def run(sender: ActorRef, msg: Any) = msg match {
+          case FlowFacade.GetMessages("akka") =>
+            sender ! messages
+            TestActor.NoAutoPilot
+        }
+      })
+
+      val request = Get("/flows/akka/messages")
+      request ~> route(httpService.ref, flowFacade.ref, flowFacadeTimeout) ~> check {
+        response.status shouldBe StatusCodes.OK
+        responseAs[Seq[Flow.Message]] shouldBe messages
+      }
+    }
+
+    "ask the FlowFacade GetMessages and respond with NotFound with the proper payload upon a 'GET /flows/unknown/messages' for an unknown Flow" in {
+      val httpService = TestProbe()
+      val flowFacade = TestProbe()
+      flowFacade.setAutoPilot(new TestActor.AutoPilot {
+        def run(sender: ActorRef, msg: Any) = msg match {
+          case FlowFacade.GetMessages("unknown") =>
+            sender ! FlowFacade.FlowUnknown("unknown")
+            TestActor.NoAutoPilot
+        }
+      })
+
+      val request = Get("/flows/unknown/messages")
+      request ~> route(httpService.ref, flowFacade.ref, flowFacadeTimeout) ~> check {
+        response.status shouldBe StatusCodes.NotFound
+      }
+    }
+
+    "ask the FlowFacade AddMessage and respond with Created with the proper payload upon a 'POST /flows/akka/messages' for an existing Flow" in {
+      val httpService = TestProbe()
+      val flowFacade = TestProbe()
+      val messageAdded = Flow.MessageAdded("akka", Flow.Message("Akka rocks!", now()))
+      flowFacade.setAutoPilot(new TestActor.AutoPilot {
+        def run(sender: ActorRef, msg: Any) = msg match {
+          case FlowFacade.AddMessage("akka", "Akka rocks!") =>
+            sender ! messageAdded
+            TestActor.NoAutoPilot
+        }
+      })
+
+      val request = Post("/flows/akka/messages", AddMessageRequest("Akka rocks!"))
+      request ~> route(httpService.ref, flowFacade.ref, flowFacadeTimeout) ~> check {
+        response.status shouldBe StatusCodes.Created
+        responseAs[Flow.MessageAdded] shouldBe messageAdded
+      }
+    }
+
+    "ask the FlowFacade AddMessage and respond with NotFound with the proper payload upon a 'POST /flows/akka/messages' for an unknown Flow" in {
+      val httpService = TestProbe()
+      val flowFacade = TestProbe()
+      val flowUnknown = FlowFacade.FlowUnknown("unknown")
+      flowFacade.setAutoPilot(new TestActor.AutoPilot {
+        def run(sender: ActorRef, msg: Any) = msg match {
+          case FlowFacade.AddMessage("unknown", "Akka rocks!") =>
+            sender ! flowUnknown
+            TestActor.NoAutoPilot
+        }
+      })
+
+      val request = Post("/flows/unknown/messages", AddMessageRequest("Akka rocks!"))
+      request ~> route(httpService.ref, flowFacade.ref, flowFacadeTimeout) ~> check {
+        response.status shouldBe StatusCodes.NotFound
+        responseAs[FlowFacade.FlowUnknown] shouldBe flowUnknown
       }
     }
   }
@@ -67,10 +243,10 @@ class HttpServiceSpec extends WordSpec with Matchers with RouteTest with Scalate
       val probe = TestProbe()
 
       val httpService = EventFilter.info(occurrences = 1, pattern = s"Listening on.*$interface:$port").intercept {
-        system.actorOf(HttpService.props(interface, port))
+        system.actorOf(HttpService.props(interface, port, system.deadLetters, flowFacadeTimeout))
       }
 
-      val otherHttpService = probe.watch(system.actorOf(HttpService.props(interface, port)))
+      val otherHttpService = probe.watch(system.actorOf(HttpService.props(interface, port, system.deadLetters, flowFacadeTimeout)))
       probe.expectTerminated(otherHttpService)
 
       probe.watch(httpService)
@@ -78,4 +254,6 @@ class HttpServiceSpec extends WordSpec with Matchers with RouteTest with Scalate
       probe.expectTerminated(httpService)
     }
   }
+
+  def now() = LocalDateTime.now().withNano(0)
 }
