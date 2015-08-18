0a21b0ec15340a893e2315ef99a8388fb58c67b6
Heiko Seeberger <mail@heikoseeberger.de>
2015-08-18 12:34:11 +0200

    Akka HTTP - reactive service

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,6 +4,7 @@ object Version {
   final val Akka         = "2.4.2"
   final val AkkaHttpJson = "1.5.2"
   final val AkkaLog4j    = "1.1.2"
+  final val AkkaSse      = "1.7.1"
   final val Circe        = "0.3.0"
   final val Log4j        = "2.5"
   final val Scala        = "2.11.8"
@@ -15,6 +16,7 @@ object Library {
   val akkaHttpCirce   = "de.heikoseeberger"        %% "akka-http-circe"        % Version.AkkaHttpJson
   val akkaHttpTestkit = "com.typesafe.akka"        %% "akka-http-testkit"      % Version.Akka
   val akkaLog4j       = "de.heikoseeberger"        %% "akka-log4j"             % Version.AkkaLog4j
+  val akkaSse         = "de.heikoseeberger"        %% "akka-sse"               % Version.AkkaSse
   val akkaTestkit     = "com.typesafe.akka"        %% "akka-testkit"           % Version.Akka
   val circeGeneric    = "io.circe"                 %% "circe-generic"          % Version.Circe
   val circeJava8      = "io.circe"                 %% "circe-java8"            % Version.Circe
