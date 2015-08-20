c8abc3623647fe9862f02e218f97bb89e0d80519
Heiko Seeberger <mail@heikoseeberger.de>
2015-08-20 19:45:24 +0200

    Akka HTTP – skeleton service

---
+++ src/main/scala/de/heikoseeberger/reactiveflows/HttpService.scala
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2015 Heiko Seeberger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.heikoseeberger.reactiveflows
+
+import akka.actor.{ Actor, ActorRef, Props, Status }
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives
+import akka.pattern.pipe
+import akka.stream.scaladsl.ImplicitMaterializer
+import de.heikoseeberger.akkamacrologging.ActorLogging
+import scala.concurrent.{ ExecutionContext, Future }
+
+object HttpService {
+
+  private[reactiveflows] case object Stop
+
+  // $COVERAGE-OFF$
+  final val Name = "http-service"
+  // $COVERAGE-ON$
+
+  def props(interface: String, port: Int): Props = Props(new HttpService(interface, port))
+
+  private[reactiveflows] def route(httpService: ActorRef)(implicit ec: ExecutionContext) = {
+    import Directives._
+
+    // format: OFF
+    def assets = getFromResourceDirectory("web") ~ pathSingleSlash(getFromResource("web/index.html"))
+
+    def stop = pathSingleSlash {
+      delete {
+        complete {
+          httpService ! Stop
+          "Stopping ..."
+        }
+      }
+    }
+    // format: ON
+
+    assets ~ stop
+  }
+}
+
+class HttpService(interface: String, port: Int) extends Actor with ActorLogging with ImplicitMaterializer {
+  import HttpService._
+  import context.dispatcher
+
+  Http(context.system)
+    .bindAndHandle(route(self), interface, port)
+    .pipeTo(self)
+
+  override def receive = binding
+
+  private def binding: Receive = {
+    case serverBinding @ Http.ServerBinding(address) =>
+      log.info("Listening on {}", address)
+      context.become(bound(serverBinding))
+
+    case Status.Failure(cause) =>
+      log.error(cause, s"Can't bind to $interface:$port")
+      context.stop(self)
+  }
+
+  private def bound(serverBinding: Http.ServerBinding): Receive = {
+    case Stop =>
+      serverBinding.unbind()
+      context.stop(self)
+  }
+}
