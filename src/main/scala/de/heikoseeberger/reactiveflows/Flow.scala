6b0c5e2f440fb41fc2498f343914170210251515
Heiko Seeberger <mail@heikoseeberger.de>
2015-08-21 10:26:46 +0200

    Akka Persistence

--- src/main/scala/de/heikoseeberger/reactiveflows/Flow.scala
+++ src/main/scala/de/heikoseeberger/reactiveflows/Flow.scala
@@ -16,9 +16,10 @@
 
 package de.heikoseeberger.reactiveflows
 
-import akka.actor.{ Actor, ActorRef, ActorSystem, Props }
+import akka.actor.{ ActorRef, ActorSystem, Props }
 import akka.cluster.pubsub.DistributedPubSubMediator
 import akka.cluster.sharding.{ ClusterSharding, ClusterShardingSettings }
+import akka.persistence.PersistentActor
 import java.time.LocalDateTime
 
 object Flow {
@@ -50,22 +51,34 @@ object Flow {
   def props(mediator: ActorRef): Props = Props(new Flow(mediator))
 }
 
-class Flow(mediator: ActorRef) extends Actor {
+class Flow(mediator: ActorRef) extends PersistentActor {
   import Flow._
+
+  private val name = self.path.name
 
   private var messages = List.empty[Message]
 
-  override def receive = {
+  override def persistenceId = s"flow-$name"
+
+  override def receiveCommand = {
     case GetMessages      => sender() ! messages
     case AddMessage(text) => addMessage(text)
     case Stop             => context.stop(self)
   }
 
-  private def addMessage(text: String) = {
-    val message = Message(text, LocalDateTime.now())
-    messages +:= message
-    val messageAdded = MessageAdded(self.path.name, message)
+  // $COVERAGE-OFF$
+  override def receiveRecover = {
+    case messageAdded: MessageAdded => updateState(messageAdded)
+  }
+  // $COVERAGE-ON$
+
+  private def addMessage(text: String) = persist(MessageAdded(name, Message(text, LocalDateTime.now()))) { messageAdded =>
+    updateState(messageAdded)
     mediator ! DistributedPubSubMediator.Publish(MessageEventKey, messageAdded)
     sender() ! messageAdded
+  }
+
+  private def updateState(event: MessageEvent) = event match {
+    case MessageAdded(flowName, message) => messages +:= message
   }
 }
