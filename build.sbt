9b94a2c1350ab422f30964d3bb64dea5171cad0e
Heiko Seeberger <mail@heikoseeberger.de>
2016-02-19 16:32:12 +0100

    Set up project (groll:initial)

--- build.sbt
+++ build.sbt
@@ -1,13 +1,36 @@
-lazy val reactiveFlows = project
-  .copy(id = "reactive-flows")
+lazy val `reactive-flows` = project
   .in(file("."))
-  .enablePlugins(AutomateHeaderPlugin, GitVersioning)
-
-name := "reactive-flows"
+  .configs(MultiJvm)
+  .enablePlugins(AutomateHeaderPlugin, GitVersioning, JavaAppPackaging, DockerPlugin)
 
 libraryDependencies ++= Vector(
-  Library.scalaCheck % "test"
+  Library.akkaClusterSharding,
+  Library.akkaDistributedData,
+  Library.akkaHttp,
+  Library.akkaHttpCirce,
+  Library.akkaLog4j,
+  Library.akkaPersistenceCassandra,
+  Library.akkaSse,
+  Library.circeGeneric,
+  Library.circeJava8,
+  Library.constructrAkka,
+  Library.constructrCoordinationEtcd,
+  Library.log4jCore,
+  Library.akkaHttpTestkit      % "test",
+  Library.akkaMultiNodeTestkit % "test",
+  Library.akkaTestkit          % "test",
+  Library.scalaTest            % "test"
 )
 
-initialCommands := """|import de.heikoseeberger.reactive.flows._
+initialCommands := """|import de.heikoseeberger.reactiveflows._
                       |""".stripMargin
+
+maintainer.in(Docker) := "Heiko Seeberger"
+daemonUser.in(Docker) := "root"
+dockerBaseImage       := "java:8"
+dockerRepository      := Some("hseeberger")
+dockerExposedPorts    := Vector(8000)
+
+addCommandAlias("rf1", "reStart -Dreactive-flows.http-service.port=8001 -Dakka.remote.netty.tcp.port=2551 -Dcassandra-journal.contact-points.0=192.168.99.100 -Dconstructr.coordination.host=192.168.99.100")
+addCommandAlias("rf2", "run     -Dreactive-flows.http-service.port=8002 -Dakka.remote.netty.tcp.port=2552 -Dcassandra-journal.contact-points.0=192.168.99.100 -Dconstructr.coordination.host=192.168.99.100")
+addCommandAlias("rf3", "run     -Dreactive-flows.http-service.port=8003 -Dakka.remote.netty.tcp.port=2553 -Dcassandra-journal.contact-points.0=192.168.99.100 -Dconstructr.coordination.host=192.168.99.100")
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,10 +1,33 @@
 import sbt._
 
 object Version {
-  final val Scala      = "2.11.7"
-  final val ScalaCheck = "1.13.0"
+  final val Akka                     = "2.4.7"
+  final val AkkaHttpJson             = "1.7.0"
+  final val AkkaLog4j                = "1.1.4"
+  final val AkkaPersistenceCassandra = "0.16"
+  final val AkkaSse                  = "1.8.1"
+  final val Circe                    = "0.4.1"
+  final val Constructr               = "0.13.2"
+  final val Log4j                    = "2.6"
+  final val Scala                    = "2.11.8"
+  final val ScalaTest                = "2.2.6"
 }
 
 object Library {
-  val scalaCheck = "org.scalacheck" %% "scalacheck" % Version.ScalaCheck
+  val akkaClusterSharding        = "com.typesafe.akka"        %% "akka-cluster-sharding"              % Version.Akka
+  val akkaDistributedData        = "com.typesafe.akka"        %% "akka-distributed-data-experimental" % Version.Akka
+  val akkaPersistenceCassandra   = "com.typesafe.akka"        %% "akka-persistence-cassandra"         % Version.AkkaPersistenceCassandra
+  val akkaHttp                   = "com.typesafe.akka"        %% "akka-http-experimental"             % Version.Akka
+  val akkaHttpCirce              = "de.heikoseeberger"        %% "akka-http-circe"                    % Version.AkkaHttpJson
+  val akkaHttpTestkit            = "com.typesafe.akka"        %% "akka-http-testkit"                  % Version.Akka
+  val akkaLog4j                  = "de.heikoseeberger"        %% "akka-log4j"                         % Version.AkkaLog4j
+  val akkaMultiNodeTestkit       = "com.typesafe.akka"        %% "akka-multi-node-testkit"            % Version.Akka
+  val akkaSse                    = "de.heikoseeberger"        %% "akka-sse"                           % Version.AkkaSse
+  val akkaTestkit                = "com.typesafe.akka"        %% "akka-testkit"                       % Version.Akka
+  val circeGeneric               = "io.circe"                 %% "circe-generic"                      % Version.Circe
+  val circeJava8                 = "io.circe"                 %% "circe-java8"                        % Version.Circe
+  val constructrAkka             = "de.heikoseeberger"        %% "constructr-akka"                    % Version.Constructr
+  val constructrCoordinationEtcd = "de.heikoseeberger"        %% "constructr-coordination-etcd"       % Version.Constructr
+  val log4jCore                  = "org.apache.logging.log4j" %  "log4j-core"                         % Version.Log4j
+  val scalaTest                  = "org.scalatest"            %% "scalatest"                          % Version.ScalaTest
 }
