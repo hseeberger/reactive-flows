28611d17627b334f7495d30bc01f37c68ef6952a
Heiko Seeberger <mail@heikoseeberger.de>
2016-02-19 16:32:12 +0100

    Set up project (groll:initial)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,10 +1,17 @@
 import sbt._
 
 object Version {
+  final val Akka       = "2.4.2"
+  final val AkkaLog4j  = "1.1.2"
+  final val Log4j      = "2.5"
   final val Scala      = "2.11.7"
-  final val ScalaCheck = "1.13.0"
+  final val ScalaTest  = "2.2.6"
 }
 
 object Library {
-  val scalaCheck = "org.scalacheck" %% "scalacheck" % Version.ScalaCheck
+  val akkaActor   = "com.typesafe.akka"        %% "akka-actor"   % Version.Akka
+  val akkaLog4j   = "de.heikoseeberger"        %% "akka-log4j"   % Version.AkkaLog4j
+  val akkaTestkit = "com.typesafe.akka"        %% "akka-testkit" % Version.Akka
+  val log4jCore   = "org.apache.logging.log4j" %  "log4j-core"   % Version.Log4j
+  val scalaTest   = "org.scalatest"            %% "scalatest"    % Version.ScalaTest
 }
