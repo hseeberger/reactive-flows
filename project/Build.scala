cb1912c95bc54233052b86437665936d8ed3313b
Heiko Seeberger <mail@heikoseeberger.de>
2016-06-24 16:34:39 +0200

    Set up project

--- project/Build.scala
+++ project/Build.scala
@@ -1,43 +1,44 @@
-import com.typesafe.sbt.{ GitPlugin, SbtScalariform }
+import com.typesafe.sbt.SbtMultiJvm.MultiJvmKeys.MultiJvm
+import com.typesafe.sbt.{GitPlugin, SbtNativePackager}
 import de.heikoseeberger.sbtheader.HeaderPlugin
 import de.heikoseeberger.sbtheader.license.Apache2_0
+import org.scalafmt.sbt.ScalaFmtPlugin
+import sbt.Keys._
 import sbt._
 import sbt.plugins.JvmPlugin
-import sbt.Keys._
-import scalariform.formatter.preferences.{ AlignSingleLineCaseStatements, DoubleIndentClassDeclaration }
 
 object Build extends AutoPlugin {
 
-  override def requires = JvmPlugin && HeaderPlugin && GitPlugin && SbtScalariform
+  override def requires = JvmPlugin && HeaderPlugin && GitPlugin && ScalaFmtPlugin && SbtNativePackager
 
   override def trigger = allRequirements
 
-  override def projectSettings = Vector(
-    // Core settings
-    organization := "de.heikoseeberger",
-    licenses += ("Apache-2.0", url("http://www.apache.org/licenses/LICENSE-2.0")),
-    scalaVersion := Version.Scala,
-    crossScalaVersions := Vector(scalaVersion.value),
-    scalacOptions ++= Vector(
-      "-unchecked",
-      "-deprecation",
-      "-language:_",
-      "-target:jvm-1.8",
-      "-encoding", "UTF-8"
-    ),
-    unmanagedSourceDirectories.in(Compile) := Vector(scalaSource.in(Compile).value),
-    unmanagedSourceDirectories.in(Test) := Vector(scalaSource.in(Test).value),
+  override def projectSettings =
+    ScalaFmtPlugin.autoImport.reformatOnCompileSettings ++
+    Vector(
+      // Core settings
+      organization := "de.heikoseeberger",
+      licenses += ("Apache-2.0", url("http://www.apache.org/licenses/LICENSE-2.0")),
+      scalaVersion := Version.Scala,
+      crossScalaVersions := Vector(scalaVersion.value),
+      scalacOptions ++= Vector(
+        "-unchecked",
+        "-deprecation",
+        "-language:_",
+        "-target:jvm-1.8",
+        "-encoding", "UTF-8"
+      ),
+      unmanagedSourceDirectories.in(Compile) := Vector(scalaSource.in(Compile).value),
+      unmanagedSourceDirectories.in(Test) := Vector(scalaSource.in(Test).value),
+      unmanagedSourceDirectories.in(MultiJvm) := Vector(scalaSource.in(MultiJvm).value),
 
-    // Scalariform settings
-    SbtScalariform.autoImport.scalariformPreferences := SbtScalariform.autoImport.scalariformPreferences.value
-      .setPreference(AlignSingleLineCaseStatements, true)
-      .setPreference(AlignSingleLineCaseStatements.MaxArrowIndent, 100)
-      .setPreference(DoubleIndentClassDeclaration, true),
+      // Scalariform settings
+      ScalaFmtPlugin.autoImport.scalafmtConfig := Some(baseDirectory.in(ThisBuild).value / ".scalafmt"),
 
-    // Git settings
-    GitPlugin.autoImport.git.useGitDescribe := true,
+      // Git settings
+      GitPlugin.autoImport.git.useGitDescribe := true,
 
-    // Header settings
-    HeaderPlugin.autoImport.headers := Map("scala" -> Apache2_0("2016", "Heiko Seeberger"))
-  )
+      // Header settings
+      HeaderPlugin.autoImport.headers := Map("scala" -> Apache2_0("2015", "Heiko Seeberger"))
+    )
 }
