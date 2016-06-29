a07f48b2a69ec901e138223b0ac0ef5f637f0b86
Heiko Seeberger <mail@heikoseeberger.de>
2016-06-29 10:27:59 +0200

    Akka Persistence

--- src/main/scala/de/heikoseeberger/reactiveflows/Flow.scala
+++ src/main/scala/de/heikoseeberger/reactiveflows/Flow.scala
@@ -16,7 +16,7 @@
 
 package de.heikoseeberger.reactiveflows
 
-import akka.actor.{ Actor, ActorLogging, ActorRef, ActorSystem, Props }
+import akka.actor.{ ActorLogging, ActorRef, ActorSystem, Props }
 import akka.cluster.pubsub.DistributedPubSubMediator.Publish
 import akka.cluster.sharding.ShardRegion.{
   ExtractEntityId,
@@ -24,6 +24,7 @@ import akka.cluster.sharding.ShardRegion.{
   Passivate
 }
 import akka.cluster.sharding.{ ClusterSharding, ClusterShardingSettings }
+import akka.persistence.PersistentActor
 import java.time.LocalDateTime
 import scala.math.{ max, min }
 
@@ -61,12 +62,16 @@ object Flow {
   }
 }
 
-final class Flow(mediator: ActorRef) extends Actor with ActorLogging {
+final class Flow(mediator: ActorRef)
+    extends PersistentActor
+    with ActorLogging {
   import Flow._
+
+  override val persistenceId = s"flow-${ self.path.name }"
 
   private var messages = Vector.empty[Message]
 
-  override def receive = {
+  override def receiveCommand = {
     case GetMessages(id, _) if id < 0        => badCommand("id < 0")
     case GetMessages(_, count) if count <= 0 => badCommand("count <= 0")
     case gm: GetMessages                     => handleGetMessages(gm)
@@ -76,6 +81,10 @@ final class Flow(mediator: ActorRef) extends Actor with ActorLogging {
 
     case Stop      => context.parent ! Passivate(Terminate)
     case Terminate => context.stop(self)
+  }
+
+  override def receiveRecover = {
+    case MessageAdded(_, message) => messages +:= message
   }
 
   private def handleGetMessages(getMessages: GetMessages) = {
@@ -89,10 +98,11 @@ final class Flow(mediator: ActorRef) extends Actor with ActorLogging {
   private def handleAddMessage(addMessage: AddMessage) = {
     import addMessage._
     val message = Message(messages.size, text, LocalDateTime.now())
-    messages +:= message
-    val messageAdded = MessageAdded(self.path.name, message)
-    mediator ! Publish(className[MessageEvent], messageAdded)
-    log.info("Message starting with '{}' added", text.take(42))
-    sender() ! messageAdded
+    persist(MessageAdded(self.path.name, message)) { messageAdded =>
+      receiveRecover(messageAdded)
+      mediator ! Publish(className[MessageEvent], messageAdded)
+      log.info("Message starting with '{}' added", text.take(42))
+      sender() ! messageAdded
+    }
   }
 }
